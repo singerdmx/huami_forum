fac929e2083a1099011cec1902811cdc56910145
Xin Yao <singerdmx@gmail.com>
2015-12-10 16:21:16 -0800

    Show mails page under profile

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -3,6 +3,9 @@ class MailsController < ApplicationController
   before_filter :authenticate_forem_user
 
   def index
+    @is_self = true
+    @is_mails_page = true
+    render '/profiles/show'
   end
 
 end
